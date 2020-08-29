ba99b37c5ec58f67edaa17450f9a85fbb448f21c
Carlos Salazar <carlosiusalazar@gmail.com>
2020-08-29 21:40:13 +0900

    Stage sides changed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let areasArray = ['Upstage Left', 'Upstage Center', 'Upstage Right','Center Stage Left','Center Stage','Center Stage Right','Downstage Left','Downstage Center', 'Downstage Right']
+let areasArray = ['Upstage Right', 'Upstage Center', 'Upstage Left','Center Stage Right','Center Stage','Center Stage Left','Downstage Right','Downstage Center', 'Downstage Left']
 
 let rightAnswer = ''
 
@@ -13,23 +13,23 @@ function playGame(){
 
 function guess(num) {
     console.log("num, rightAnswer", num,rightAnswer)
-   if (num === 1 && rightAnswer === "Upstage Left"){
+   if (num === 1 && rightAnswer === "Upstage Right"){
        alert("Correct!, Try another?")
    } else if (num === 2 && rightAnswer === "Upstage Center") {
     alert("Correct!, Try another?")
-   } else if (num === 3 && rightAnswer === "Upstage Right") {
+   } else if (num === 3 && rightAnswer === "Upstage Left") {
     alert("Correct!, Try another?")
-   } else if (num === 4 && rightAnswer === "Center Stage Left") {
+   } else if (num === 4 && rightAnswer === "Center Stage Right") {
     alert("Correct!, Try another?")
    } else if (num === 5 && rightAnswer === "Center Stage") {
     alert("Correct!, Try another?")
-   } else if (num === 6 && rightAnswer === "Center Stage Right") {
+   } else if (num === 6 && rightAnswer === "Center Stage Left") {
     alert("Correct!, Try another?")
-   } else if (num === 7 && rightAnswer === "Downstage Left") {
+   } else if (num === 7 && rightAnswer === "Downstage Right") {
     alert("Correct!, Try another?")
    } else if (num === 8 && rightAnswer === "Downstage Center") {
     alert("Correct!, Try another?")
-   } else if (num === 9 && rightAnswer === "Downstage Right") {
+   } else if (num === 9 && rightAnswer === "Downstage Left") {
     alert("Correct!, Try another?")
    } else {
        alert("Sorry, that's " + areasArray[num-1] + ". Try again?")
